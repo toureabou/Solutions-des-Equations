2d7ea4d7a67fd8fd69c2c83b26a736cfd06f5f24
Abou Toure <toureabou365@gmail.com>
2022-01-15 10:19:27 +0100

    Update Solutions Equations.cpp

--- Solutions Equations.cpp
+++ Solutions Equations.cpp
@@ -8,7 +8,7 @@ int main()
 	cout << "Entrer les 3 coefficients : ";
 	cin >> a>> b>> c;
 	
-	d = b^2 - 4*a*c;
+	d = pow(b,2) - 4*a*c;
 	
 	 	if( d < 0)
 	 	{
@@ -16,7 +16,7 @@ int main()
 		}
 		
 		else 
-			if(d = 0)
+			if(d == 0)
 			{
 			int x = -b/ 2*a;
 			cout << "La solution de l'equation est : " << x;
